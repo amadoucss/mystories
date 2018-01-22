f63acb9fad72cb646b184cd333f2f0a900e73b6c
Ams <amadoucss72@gmail.com>
2018-01-22 23:57:21 +0200

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', function(req, res){
+	res.send('works');
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+console.log('server listening at port'+ port)
+})
