6adee358b2b015290d02a503f7d3d2868835ebc1
Ams <amadoucss72@gmail.com>
2018-01-23 09:41:42 +0200

    passport oAuth

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const passport = require('passport');
 
 const app = express();
+
+require('./config/passport')(passport)
+
+app.use('/auth', authRoutes);
+
+
 
 app.get('/', function(req, res){
 	res.send('hello world');
 })
+
+
 
 const port = process.env.PORT || 3000;
 
